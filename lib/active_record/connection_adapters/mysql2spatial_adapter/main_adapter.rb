3eb76a506dc48d28ad69316373d605a29a3426a1
Janusz Mordarski <janusz.mordarski@montrosesoftware.com>
2015-08-06 21:00:03 +0200

    Fix indexes method - use SpatialIndexDefinition only for type :spatial

--- lib/active_record/connection_adapters/mysql2spatial_adapter/main_adapter.rb
+++ lib/active_record/connection_adapters/mysql2spatial_adapter/main_adapter.rb
@@ -120,7 +120,7 @@ module ActiveRecord
           columns_
         end
 
-
+        # Returns an array of indexes for the given table.
         def indexes(table_name_, name_=nil)
           indexes_ = []
           current_index_ = nil
@@ -129,11 +129,20 @@ module ActiveRecord
             if current_index_ != row_[:Key_name]
               next if row_[:Key_name] == 'PRIMARY' # skip the primary key
               current_index_ = row_[:Key_name]
-              indexes_ << ::RGeo::ActiveRecord::SpatialIndexDefinition.new(row_[:Table], row_[:Key_name], row_[:Non_unique] == 0, [], [], row_[:Index_type] == 'SPATIAL')
+              mysql_index_type = row_[:Index_type].downcase.to_sym
+              index_type  = INDEX_TYPES.include?(mysql_index_type)  ? mysql_index_type : nil
+              index_using = INDEX_USINGS.include?(mysql_index_type) ? mysql_index_type : nil
+              options = [row_[:Table], row_[:Key_name], row_[:Non_unique].to_i == 0, [], [], nil, nil, index_type, index_using]
+              indexes_ << if mysql_index_type == :spatial
+                options.push(true)
+                ::RGeo::ActiveRecord::SpatialIndexDefinition.new(*options)
+              else
+                IndexDefinition.new(*options)
+              end
             end
             last_index_ = indexes_.last
             last_index_.columns << row_[:Column_name]
-            last_index_.lengths << row_[:Sub_part] unless last_index_.spatial
+            last_index_.lengths << row_[:Sub_part] unless mysql_index_type == :spatial
           end
           indexes_
         end
