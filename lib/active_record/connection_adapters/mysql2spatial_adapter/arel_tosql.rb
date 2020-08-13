f4d9b0bfbdb9b8fb1267b6a058ebb567ce222d4f
ZC-Loaner <zc-loaner@ZC-Loaners-MBP.Dlink>
2020-08-13 16:21:04 +0530

    Changed GeomFromWKB to ST_GeomFromWKB, as GeomFromWKB is deprecated in and after mysql5.7

--- lib/active_record/connection_adapters/mysql2spatial_adapter/arel_tosql.rb
+++ lib/active_record/connection_adapters/mysql2spatial_adapter/arel_tosql.rb
@@ -39,9 +39,9 @@ module Arel
       end
 
       FUNC_MAP = {
-        'st_wkttosql' => 'GeomFromText',
-        'st_wkbtosql' => 'GeomFromWKB',
-        'st_length' => 'GLength'
+        'st_wkttosql' => 'ST_GeomFromText',
+        'st_wkbtosql' => 'ST_GeomFromWKB',
+        'st_length' => 'ST_GLength'
       }.freeze
 
       include ::RGeo::ActiveRecord::SpatialToSql
